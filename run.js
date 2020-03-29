70abaddfb66ba32944b9e98a1d705abaea3c1a11
Justin Lui <jlui@surveymonkey.com>
2020-03-29 11:33:48 -0700

    back to start

--- run.js
+++ run.js
@@ -34,80 +34,8 @@ jack,
 
 let resultCSV = ''
 
-const sortbyName = (nameA, nameB) => {
-  const nameAUpper = nameA.toUpperCase()
-  const nameBUpper = nameB.toUpperCase()
-
-  let comparison = 0
-  if (nameAUpper > nameBUpper) {
-    comparison = 1
-  } else if (nameAUpper < nameBUpper) {
-    comparison = -1
-  }
-  return comparison
-}
-
 const buildOrgChart = (data) => {
   // Put your implementation here:
-  if (
-    data.every((row) => {
-      return !!row.manager
-    })
-  ) {
-    throw new Error('no top level manager found')
-  }
-
-  const topLevelManager = data.filter((row) => row.manager === '')[0]
-
-  const getDirectReports = (manager) => {
-    return data
-      .filter((row) => row.manager === manager)
-      .sort((a, b) => sortbyName(a.employee, b.employee))
-  }
-
-  const resultList = []
-
-  // CSV GENERATOR PSEUDOCODE
-  // for a given employee
-  //    get his direct reports
-  //    if direct report has no suboridinates
-  //         print out row
-  //    else
-  //         get the subordinates of the direct report and repeat process
-
-  // PATTERN
-  // declare some variables to store some state outside of the recursive function
-  // write recursive function that will mutate those outside variables
-  // there should be some logic between the variables in the recursive function
-  // and the outside variables that will dictate the base case of the recursion
-  // Or in other words, it will determine when to break out of recursion
-
-  const printList = (employee, nesting = 0) => {
-    const currentNesting = nesting
-    const commaPrefixString = ''
-      ? nesting === 0
-      : [...Array(nesting)].map(() => ',').join('')
-
-    while (getDirectReports(employee).length > 0) {
-      console.log(
-        `printList, inside while loop, employee=${employee}, nesting=${nesting}`
-      )
-
-      const drs = getDirectReports(employee)
-
-      resultList.push(`${commaPrefixString}${employee}`)
-      drs.forEach(({ employee }) => printList(employee, currentNesting + 1))
-      return
-    }
-
-    console.log(
-      `printList, out of while loop, employee=${employee} nesting=${nesting}`
-    )
-    resultList.push(`${commaPrefixString}${employee}`)
-  }
-
-  printList(topLevelManager.employee)
-  resultCSV = resultList.join('\n')
 }
 
 // Harness for reading an input CSV file into an array
